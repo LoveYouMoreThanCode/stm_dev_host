08fb41118e6fbc75cf547c5fd1a281c912a5a259
fanwei <fanwei0512@qq.com>
2024-03-08 21:36:22 +0800

    tmp save to config uart1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "log.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
